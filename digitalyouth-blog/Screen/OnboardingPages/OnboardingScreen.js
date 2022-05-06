0244be664ce5ed0c033da923c0739d75b6c197a5
Terence-Fonji <terencehilln@gmail.com>
2022-05-06 21:18:47 +0200

    OnboardingPages Starting

---
+++ digitalyouth-blog/Screen/OnboardingPages/OnboardingScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    SafeAreaView,
+    Image,
+    StyleSheet,
+    FlatList,
+    StatusBar,
+    TouchableOpacity,
+    Dimensions
+} from 'react-native'
+
+const OnboardingScreen = () => {
+  return (
+      <View>
+          
+    </View>
+  )
+}
+
+export default OnboardingScreen;
